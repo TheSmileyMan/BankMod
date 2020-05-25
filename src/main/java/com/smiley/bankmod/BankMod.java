4f221585b17ed4418a4a363838e90d3b96d7b633
TheSmileyMan <jesusperalta1026@gmail.com>
2020-05-25 11:40:53 -0600

    Just Changed a few stuff.

--- src/main/java/com/smiley/bankmod/BankMod.java
+++ src/main/java/com/smiley/bankmod/BankMod.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package com.smiley.bankmod;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
@@ -19,14 +19,14 @@ import org.apache.logging.log4j.Logger;
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("examplemod")
-public class ExampleMod
+@Mod("bankmod")
+public class BankMod
 {
-    // Directly reference a log4j logger.
     private static final Logger LOGGER = LogManager.getLogger();
 
-    public ExampleMod() {
-        // Register the setup method for modloading
+    public BankMod() {
+        FMLJavaModLoadingContext.get().getModEventBus().register(this);
+
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
         // Register the enqueueIMC method for modloading
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::enqueueIMC);
