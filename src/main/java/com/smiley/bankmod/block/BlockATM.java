809a9bab2f7abb1cf1b0bb2cfb55d15a19bfe7ff
TheSmileyMan <jesusperalta1026@gmail.com>
2020-05-30 11:32:14 -0600

    Sort of added Two Block High

--- src/main/java/com/smiley/bankmod/block/BlockATM.java
+++ src/main/java/com/smiley/bankmod/block/BlockATM.java
@@ -2,18 +2,25 @@ package com.smiley.bankmod.block;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.HorizontalBlock;
 import net.minecraft.item.BlockItemUseContext;
 import net.minecraft.state.DirectionProperty;
+import net.minecraft.state.EnumProperty;
 import net.minecraft.state.StateContainer;
 import net.minecraft.state.properties.BlockStateProperties;
+import net.minecraft.state.properties.DoubleBlockHalf;
 import net.minecraft.util.Direction;
 import net.minecraft.util.Mirror;
 import net.minecraft.util.Rotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.world.IBlockReader;
 
 public class BlockATM extends Block
 {
     public static final DirectionProperty FACING = BlockStateProperties.HORIZONTAL_FACING;
+    public static final EnumProperty<DoubleBlockHalf> HALF = BlockStateProperties.DOUBLE_BLOCK_HALF;
+    protected static final VoxelShape SHAPES = Block.makeCuboidShape(0, 0, 0, 16, 32, 16);
 
     public BlockATM(Properties properties)
     {
@@ -21,6 +28,7 @@ public class BlockATM extends Block
         this.setDefaultState(this.stateContainer.getBaseState().with(FACING, Direction.NORTH));
     }
 
+    //Everything for the Direction of the block.
     @Override
     public BlockState getStateForPlacement(BlockItemUseContext context)
     {
@@ -46,7 +54,23 @@ public class BlockATM extends Block
         builder.add(FACING);
     }
 
+    // Everything to make it into two block high
 
+
+    @Override
+    public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPES;
+    }
+
+    @Override
+    public VoxelShape getRenderShape(BlockState state, IBlockReader worldIn, BlockPos pos) {
+        return SHAPES;
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+        return SHAPES;
+    }
 
     /*
     @Override
